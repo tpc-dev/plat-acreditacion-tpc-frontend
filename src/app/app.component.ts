2fc1975ea8e15a99c64bdfd1ade345741cd4ae6d
Sebastian Manriquez <seba.manriquezh@gmail.com>
2021-10-08 22:08:32 -0300

    cambios vista login y pre cambio hojas de estilo css to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { HttpHeaders } from '@angular/common/http';
 import { AuthService } from './core/services/auth.service';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Breakpoints, BreakpointState, BreakpointObserver } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,17 +15,55 @@ import { AuthService } from './core/services/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
   title = 'plat-acreditacion-tpc-frontend';
   showFiller: boolean = false
   header = new HttpHeaders();
   isLoggedIn: boolean = false;
-  constructor(private http: HttpClient, private authService: AuthService) {
+  constructor(private http: HttpClient, private authService: AuthService, private observer: BreakpointObserver,
+    private cdref: ChangeDetectorRef, public router: Router) {
+
+    // this.header.set('Access-Control-Allow-Origin', '*');
+    // this.http.get('https://localhost:44385/api/administrador-contrato-externo', { headers: this.header }).subscribe((response) => {
+    //   console.log(response)
+    // })
+  }
+
+  ngAfterViewInit(): void {
+    this.observeSession();
+    this.observer.observe(['(max-width:800px)']).subscribe((res) => {
+      if (!this.sidenav) return;
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+      this.cdref.detectChanges();
+    })
+  }
+
+  observeSession() {
     this.authService.isLoggedInObservable().subscribe(status => {
       this.isLoggedIn = status;
+      this.cdref.detectChanges();
+      if (!this.sidenav) return;
+      if (this.isLoggedIn) {
+        console.log("iniciando sesion");
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      } else {
+        console.log("cerrando sesion");
+      }
     })
-    this.header.set('Access-Control-Allow-Origin', '*');
-    this.http.get('https://localhost:44385/api/administrador-contrato-externo', { headers: this.header }).subscribe((response) => {
-      console.log(response)
-    })
+  }
+
+  logout(): void {
+    this.authService.setSession(false);
+    this.router.navigate(['/login-tpc'])
   }
 }
--- src/app/pages/logintpc-page/logintpc-page.component.ts
+++ src/app/pages/logintpc-page/logintpc-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-logintpc',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogintpcPageComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(public formBuilder: FormBuilder, public authService: AuthService, public router: Router) {
+    this.loginForm = this.createloginForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  doLogin(): void {
+    this.authService.sessionOn.next(true);
+    this.router.navigateByUrl('/home')
+    this.loginForm.markAllAsTouched();
+    if (!this.loginForm.valid) {
+      console.warn('complete datos validos');
+
+    }
+
+  }
+
+  createloginForm() {
+    return this.formBuilder.group({
+      username: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.email,
+          Validators.maxLength(20),
+        ])
+      ),
+      password: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(15),
+        ])
+      ),
+    });
   }
 
 }
